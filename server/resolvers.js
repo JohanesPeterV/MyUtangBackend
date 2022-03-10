b1684b947377723fa7826d929c5b8f007cb6a973
JopHme <60497959+JopHme@users.noreply.github.com>
2022-03-10 09:24:29 +0700

    added try catch for change username

--- server/resolvers.js
+++ server/resolvers.js
@@ -297,15 +297,19 @@ const resolvers = {
                             );
 
                         }
-                    ).catch((e)=>{
-                        throw  MyUtangError(e.message,'ConstraintError');
+                    ).catch((e) => {
+                        throw  MyUtangError(e.message, 'ConstraintError');
                     })
                 }
 
 
-                const data = await updateUser();
-                return data[1];
+                try {
+                    const data = await updateUser();
+                    return data[1];
 
+                } catch (e) {
+                    throw  MyUtangError(e.message, 'ConstraintError');
+                }
             },
 
             async updateDebt(root, {debtId, debtorId, title, description, amount}, context) {
